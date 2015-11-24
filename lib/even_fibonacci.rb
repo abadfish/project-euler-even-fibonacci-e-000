ce14e5edf3b937f5459877f440f91575d9ad1de6
abadfish <kirsten.ofarrell@gmail.com>
2015-11-23 20:32:00 -0500

    Done.

--- lib/even_fibonacci.rb
+++ lib/even_fibonacci.rb
@@ -1,1 +1,43 @@
 # Implement your procedural solution here!
+
+def even_fibonacci_sum(limit)
+  current = 2
+  previous = 1
+  fibs = [1, 2]
+  evens = []
+  while current + previous < limit
+    previous, current = current, current + previous
+    fibs.push(current)
+  end
+   
+  fibs.each do |i|
+    if i % 2 == 0 
+      evens.push(i)
+    end
+  end
+  return evens.reduce(:+)
+end
+
+
+# def evens_fibonacci_sum(n)
+#   sum = 0
+#   previous = 0
+#   i=1
+#   while i < 4000000
+#     sum +=i if (i % 2 == 0)
+#     i, previous = previous, previous + i
+#   end
+# end
+
+# puts sum
+
+
+
+# def fibonacci(n)
+#   return n if n <= 1
+#   fibonacci(n-1) + fibonacci(n-2)
+# end
+
+
+
+
--- lib/oo_even_fibonacci.rb
+++ lib/oo_even_fibonacci.rb
@@ -1,1 +1,35 @@
 # Implement your object-oriented solution here!
+
+class EvenFibonacci
+
+  def initialize(limit)
+    @limit = limit
+  end
+
+
+  def limit(limit)
+    @limit = limit
+  end
+
+  def limit
+    @limit
+  end
+
+  def sum
+    current = 2
+    previous = 1
+    fibs = [1, 2]
+    evens = []
+    while current + previous < @limit
+      previous, current = current, current + previous
+      fibs.push(current)
+    end
+   
+    fibs.each do |i|
+      if i % 2 == 0 
+        evens.push(i)
+      end
+    end
+    return evens.reduce(:+)
+  end
+end
